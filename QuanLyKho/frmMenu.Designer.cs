113da2e6b096ca3291900b7762adea32be33fea8
trivan77 <o868994610@gmail.com>
2020-12-23 02:55:39 +0700

    TriCheckXong

--- QuanLyKho/frmMenu.Designer.cs
+++ QuanLyKho/frmMenu.Designer.cs
@@ -28,21 +28,227 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // frmMenu
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1304, 647);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.Name = "frmMenu";
-            this.Text = "frmMenu";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
-            this.ResumeLayout(false);
+               this.btnthoat = new System.Windows.Forms.Button();
+               this.txttk = new System.Windows.Forms.TextBox();
+               this.btndangnhap = new System.Windows.Forms.Button();
+               this.txtmatkhau = new System.Windows.Forms.TextBox();
+               this.checkmk = new System.Windows.Forms.CheckBox();
+               this.label1 = new System.Windows.Forms.Label();
+               this.tabPage1 = new System.Windows.Forms.TabPage();
+               this.txttaikhoan = new System.Windows.Forms.GroupBox();
+               this.label2 = new System.Windows.Forms.Label();
+               this.tabControl1 = new System.Windows.Forms.TabControl();
+               this.đổiMậtKhẩuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+               this.thêmTàiKhoảnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+               this.btnTaiKhoan = new System.Windows.Forms.ToolStripMenuItem();
+               this.btnThongKe = new System.Windows.Forms.ToolStripMenuItem();
+               this.btnXuatKho = new System.Windows.Forms.ToolStripMenuItem();
+               this.btnNhapKho = new System.Windows.Forms.ToolStripMenuItem();
+               this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+               this.tabPage1.SuspendLayout();
+               this.txttaikhoan.SuspendLayout();
+               this.tabControl1.SuspendLayout();
+               this.menuStrip1.SuspendLayout();
+               this.SuspendLayout();
+               // 
+               // btnthoat
+               // 
+               this.btnthoat.Location = new System.Drawing.Point(323, 183);
+               this.btnthoat.Name = "btnthoat";
+               this.btnthoat.Size = new System.Drawing.Size(75, 28);
+               this.btnthoat.TabIndex = 6;
+               this.btnthoat.Text = "Thoát";
+               this.btnthoat.UseVisualStyleBackColor = true;
+               this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+               // 
+               // txttk
+               // 
+               this.txttk.Location = new System.Drawing.Point(203, 67);
+               this.txttk.Name = "txttk";
+               this.txttk.Size = new System.Drawing.Size(195, 20);
+               this.txttk.TabIndex = 3;
+               // 
+               // btndangnhap
+               // 
+               this.btndangnhap.Location = new System.Drawing.Point(203, 185);
+               this.btndangnhap.Name = "btndangnhap";
+               this.btndangnhap.Size = new System.Drawing.Size(75, 26);
+               this.btndangnhap.TabIndex = 5;
+               this.btndangnhap.Text = "Đăng Nhập";
+               this.btndangnhap.UseVisualStyleBackColor = true;
+               this.btndangnhap.Click += new System.EventHandler(this.btndangnhap_Click);
+               // 
+               // txtmatkhau
+               // 
+               this.txtmatkhau.Location = new System.Drawing.Point(203, 123);
+               this.txtmatkhau.Name = "txtmatkhau";
+               this.txtmatkhau.Size = new System.Drawing.Size(195, 20);
+               this.txtmatkhau.TabIndex = 4;
+               // 
+               // checkmk
+               // 
+               this.checkmk.AutoSize = true;
+               this.checkmk.Location = new System.Drawing.Point(76, 189);
+               this.checkmk.Name = "checkmk";
+               this.checkmk.Size = new System.Drawing.Size(66, 17);
+               this.checkmk.TabIndex = 2;
+               this.checkmk.Text = "Hiển Thị";
+               this.checkmk.UseVisualStyleBackColor = true;
+               this.checkmk.CheckedChanged += new System.EventHandler(this.checkmk_CheckedChanged);
+               // 
+               // label1
+               // 
+               this.label1.AutoSize = true;
+               this.label1.Location = new System.Drawing.Point(73, 67);
+               this.label1.Name = "label1";
+               this.label1.Size = new System.Drawing.Size(56, 13);
+               this.label1.TabIndex = 0;
+               this.label1.Text = "Tài Khoản";
+               // 
+               // tabPage1
+               // 
+               this.tabPage1.Controls.Add(this.txttaikhoan);
+               this.tabPage1.Location = new System.Drawing.Point(4, 22);
+               this.tabPage1.Name = "tabPage1";
+               this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+               this.tabPage1.Size = new System.Drawing.Size(970, 476);
+               this.tabPage1.TabIndex = 0;
+               this.tabPage1.UseVisualStyleBackColor = true;
+               // 
+               // txttaikhoan
+               // 
+               this.txttaikhoan.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+               this.txttaikhoan.BackColor = System.Drawing.Color.DarkKhaki;
+               this.txttaikhoan.Controls.Add(this.btnthoat);
+               this.txttaikhoan.Controls.Add(this.txttk);
+               this.txttaikhoan.Controls.Add(this.btndangnhap);
+               this.txttaikhoan.Controls.Add(this.txtmatkhau);
+               this.txttaikhoan.Controls.Add(this.checkmk);
+               this.txttaikhoan.Controls.Add(this.label2);
+               this.txttaikhoan.Controls.Add(this.label1);
+               this.txttaikhoan.Location = new System.Drawing.Point(244, 86);
+               this.txttaikhoan.Name = "txttaikhoan";
+               this.txttaikhoan.Size = new System.Drawing.Size(508, 276);
+               this.txttaikhoan.TabIndex = 1;
+               this.txttaikhoan.TabStop = false;
+               // 
+               // label2
+               // 
+               this.label2.AutoSize = true;
+               this.label2.Location = new System.Drawing.Point(73, 126);
+               this.label2.Name = "label2";
+               this.label2.Size = new System.Drawing.Size(53, 13);
+               this.label2.TabIndex = 1;
+               this.label2.Text = "Mật Khẩu";
+               // 
+               // tabControl1
+               // 
+               this.tabControl1.Controls.Add(this.tabPage1);
+               this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+               this.tabControl1.Location = new System.Drawing.Point(0, 24);
+               this.tabControl1.Name = "tabControl1";
+               this.tabControl1.SelectedIndex = 0;
+               this.tabControl1.Size = new System.Drawing.Size(978, 502);
+               this.tabControl1.TabIndex = 3;
+               // 
+               // đổiMậtKhẩuToolStripMenuItem
+               // 
+               this.đổiMậtKhẩuToolStripMenuItem.Name = "đổiMậtKhẩuToolStripMenuItem";
+               this.đổiMậtKhẩuToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+               this.đổiMậtKhẩuToolStripMenuItem.Text = "Đổi Mật Khẩu";
+               this.đổiMậtKhẩuToolStripMenuItem.Click += new System.EventHandler(this.đổiMậtKhẩuToolStripMenuItem_Click);
+               // 
+               // thêmTàiKhoảnToolStripMenuItem
+               // 
+               this.thêmTàiKhoảnToolStripMenuItem.Name = "thêmTàiKhoảnToolStripMenuItem";
+               this.thêmTàiKhoảnToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+               this.thêmTàiKhoảnToolStripMenuItem.Text = "Thêm Tài Khoản";
+               this.thêmTàiKhoảnToolStripMenuItem.Click += new System.EventHandler(this.thêmTàiKhoảnToolStripMenuItem_Click);
+               // 
+               // btnTaiKhoan
+               // 
+               this.btnTaiKhoan.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.thêmTàiKhoảnToolStripMenuItem,
+            this.đổiMậtKhẩuToolStripMenuItem});
+               this.btnTaiKhoan.Name = "btnTaiKhoan";
+               this.btnTaiKhoan.Size = new System.Drawing.Size(70, 20);
+               this.btnTaiKhoan.Text = "Tài Khoản";
+               // 
+               // btnThongKe
+               // 
+               this.btnThongKe.Name = "btnThongKe";
+               this.btnThongKe.Size = new System.Drawing.Size(69, 20);
+               this.btnThongKe.Text = "Thống Kê";
+               this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+               // 
+               // btnXuatKho
+               // 
+               this.btnXuatKho.Name = "btnXuatKho";
+               this.btnXuatKho.Size = new System.Drawing.Size(67, 20);
+               this.btnXuatKho.Text = "Xuất Kho";
+               this.btnXuatKho.Click += new System.EventHandler(this.btnXuatKho_Click);
+               // 
+               // btnNhapKho
+               // 
+               this.btnNhapKho.Name = "btnNhapKho";
+               this.btnNhapKho.Size = new System.Drawing.Size(72, 20);
+               this.btnNhapKho.Text = "Nhập Kho";
+               this.btnNhapKho.Click += new System.EventHandler(this.btnNhapKho_Click);
+               // 
+               // menuStrip1
+               // 
+               this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNhapKho,
+            this.btnXuatKho,
+            this.btnThongKe,
+            this.btnTaiKhoan});
+               this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+               this.menuStrip1.Name = "menuStrip1";
+               this.menuStrip1.Size = new System.Drawing.Size(978, 24);
+               this.menuStrip1.TabIndex = 2;
+               this.menuStrip1.Text = "menuStrip1";
+               // 
+               // frmMenu
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(978, 526);
+               this.Controls.Add(this.tabControl1);
+               this.Controls.Add(this.menuStrip1);
+               this.Name = "frmMenu";
+               this.Text = "frmMenu";
+               this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+               this.tabPage1.ResumeLayout(false);
+               this.txttaikhoan.ResumeLayout(false);
+               this.txttaikhoan.PerformLayout();
+               this.tabControl1.ResumeLayout(false);
+               this.menuStrip1.ResumeLayout(false);
+               this.menuStrip1.PerformLayout();
+               this.ResumeLayout(false);
+               this.PerformLayout();
 
         }
 
-        #endregion
-    }
+          #endregion
+
+          private System.Windows.Forms.Button btnthoat;
+          private System.Windows.Forms.TextBox txttk;
+          private System.Windows.Forms.Button btndangnhap;
+          private System.Windows.Forms.TextBox txtmatkhau;
+          private System.Windows.Forms.CheckBox checkmk;
+          private System.Windows.Forms.Label label1;
+          private System.Windows.Forms.TabPage tabPage1;
+          private System.Windows.Forms.GroupBox txttaikhoan;
+          private System.Windows.Forms.Label label2;
+          private System.Windows.Forms.TabControl tabControl1;
+          private System.Windows.Forms.ToolStripMenuItem đổiMậtKhẩuToolStripMenuItem;
+          private System.Windows.Forms.ToolStripMenuItem thêmTàiKhoảnToolStripMenuItem;
+          private System.Windows.Forms.ToolStripMenuItem btnTaiKhoan;
+          private System.Windows.Forms.ToolStripMenuItem btnThongKe;
+          private System.Windows.Forms.ToolStripMenuItem btnXuatKho;
+          private System.Windows.Forms.ToolStripMenuItem btnNhapKho;
+          private System.Windows.Forms.MenuStrip menuStrip1;
+     }
 }
--- QuanLyKho/frmMenu.cs
+++ QuanLyKho/frmMenu.cs
@@ -14,12 +14,118 @@ namespace QuanLyKho
 {
     public partial class frmMenu : Form
     {
-        public frmMenu()
+          TabPage tabPage2 = new TabPage();
+          TabPage tabPage3 = new TabPage();
+          TabPage tabPage4 = new TabPage();
+          TabPage tabPage5 = new TabPage();
+
+          public frmMenu()
         {
             InitializeComponent();
           
             
         }
-       
-    } 
+
+          public void remove()
+          {
+               tabControl1.TabPages.Remove(tabPage2);
+               tabControl1.TabPages.Remove(tabPage3);
+               tabControl1.TabPages.Remove(tabPage4);
+               tabControl1.TabPages.Remove(tabPage5);
+          }
+
+          private void checkmk_CheckedChanged(object sender, EventArgs e)
+          {
+               if (txtmatkhau.UseSystemPasswordChar == true)
+               {
+                    txtmatkhau.UseSystemPasswordChar = false;
+               }
+               else
+               {
+                    txtmatkhau.UseSystemPasswordChar = true;
+               }
+          }
+
+          public void hien()
+          {
+               btnNhapKho.Enabled = true;
+               btnXuatKho.Enabled = true;
+               btnThongKe.Enabled = true;
+               btnTaiKhoan.Enabled = true;
+               
+          }
+
+          private void btndangnhap_Click(object sender, EventArgs e)
+          {
+               if (TaiKhoanDAO.DangNhap(txttk.Text, txtmatkhau.Text) == true)
+               {
+                    tabControl1.TabPages.Remove(tabPage1);
+                    hien();
+               }
+               else
+               {
+                    MessageBox.Show("Đăng nhập thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txttaikhoan.Focus();
+               }
+          }
+
+          private void btnthoat_Click(object sender, EventArgs e)
+          {
+               if (MessageBox.Show("Bạn có muốn thoát không ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+               {
+                    Application.Exit();
+               }
+          }
+
+          private void btnXuatKho_Click(object sender, EventArgs e)
+          {
+               remove();
+               frmXuatKho f = new frmXuatKho();
+               f.TopLevel = false;
+               f.FormBorderStyle = FormBorderStyle.None;
+               f.Dock = DockStyle.Fill;
+               tabPage3.Controls.Add(f);
+               f.Visible = true;
+               tabPage3.Text = "Xuất Kho";
+               tabControl1.TabPages.Add(tabPage3);
+          }
+
+          private void btnNhapKho_Click(object sender, EventArgs e)
+          {
+               remove();
+               frmNhapKho f = new frmNhapKho();
+               f.TopLevel = false;
+               f.FormBorderStyle = FormBorderStyle.None;
+               f.Dock = DockStyle.Fill;
+               tabPage2.Controls.Add(f);
+               f.Visible = true;
+               tabPage2.Text = "Nhập Kho";
+               tabControl1.TabPages.Add(tabPage2);
+          }
+
+          private void btnThongKe_Click(object sender, EventArgs e)
+          {
+               remove();
+               frmThongKe f = new frmThongKe();
+               f.TopLevel = false;
+               f.FormBorderStyle = FormBorderStyle.None;
+               f.Dock = DockStyle.Fill;
+               tabPage4.Controls.Add(f);
+               f.Visible = true;
+               tabPage4.Text = "Thống Kê";
+               tabControl1.TabPages.Add(tabPage4);
+          }
+
+          private void thêmTàiKhoảnToolStripMenuItem_Click(object sender, EventArgs e)
+          {
+               frmThemTaiKhoan f = new frmThemTaiKhoan();
+               f.ShowDialog();
+          }
+
+          private void đổiMậtKhẩuToolStripMenuItem_Click(object sender, EventArgs e)
+          {
+               frmDoiMatKhau f = new frmDoiMatKhau();
+               f.ShowDialog();
+          }
+     } 
 }   
